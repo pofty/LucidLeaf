3643737b699b412417c5aebfbbbe1d635a5ffe06
Hasan Wazzan <info@hasanwazzan.com>
2024-03-16 23:57:52 +0000

    dynamic buttons values

---
+++ frontend/src/footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', marginTop: '20px', backgroundColor: '#f0f0f0' }}>
+      © 2024 Your Company Name. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
