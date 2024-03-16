8775be5d0a82a3ec85711a0bd2871d6b6892f084
Hasan Wazzan <info@hasanwazzan.com>
2024-03-16 22:37:30 +0000

    Making the skelton upload function work, and show the returned ipfs link

--- frontend/src/lighthouse/UploadText.js
+++ frontend/src/lighthouse/UploadText.js
@@ -1,24 +1,13 @@
 import lighthouse from '@lighthouse-web3/sdk'
 import {ApiKeys} from "../constants/ApiKeys.js";
-import {JournalType} from "../constants/JournalType.js";
 
-/**
-* Use this function to upload an encrypted text string to IPFS.
-*
-* @param {string} text - The text you want to upload.
-* @param {string} apiKey - Your unique Lighthouse API key.
-* @param {string} publicKey - Your wallet's public key.
-* @param {string} signedMessage - A message you've signed using your private key.
-* @param {string} [name] - optional name for text
-*
-* @return {object} - Details of the uploaded file on IPFS.
-*/
+export async function uploadText(inputText, journalType) {
+    const apiKey = ApiKeys.LIGHTHOUSE_V1.toString()
+    const response = await lighthouse.uploadText(inputText, apiKey, journalType)
+    console.log("response from the lighthouse uploadText function: ", response)
 
-const yourText = "Hello LucidLeaf"
-const apiKey = ApiKeys.LIGHTHOUSE_V1.toString()
-const publicKey = '0x477D6d7B75D6bb2a0e4B9e6339ae07374cfFaA27'
-const signedMessage = "SIGNATURE/JWT"
-const name = "anime"
+    // return the IPFS hash
+    console.log("response.data.Hash: ", response.data.Hash)
+    return response.data.Hash;
+}
 
-const response = await lighthouse.textUploadEncrypted(yourText, apiKey, publicKey, signedMessage)
-console.log(response)
