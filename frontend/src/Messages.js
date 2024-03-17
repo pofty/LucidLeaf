5d60d783b6602d986bc26c22e7a9356f9d53b313
larfef <kerstensreda@outlook.com>
2024-03-17 01:27:55 +0100

    Add drop down menu in Message.js and GetRecord button

--- frontend/src/Messages.js
+++ frontend/src/Messages.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Messages.css';
+import { JournalType } from './constants/JournalType.js';
 
 function Messages() {
-	return (
-		<div className="Container">
-			<div className="Messages">Messages</div>
-		</div>
-	)
+    const [selectedType, setSelectedType] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedType(event.target.value);
+    };
+
+    return (
+        <div className="Container">
+            <div className="JournalType">
+                <label htmlFor="journalType">Choose a Journal Type: </label>
+                <select id="journalType" value={selectedType} onChange={handleChange}>
+                    <option value="">Select</option>
+                    {Object.values(JournalType).map((type, index) => (
+                        <option key={index} value={type}>
+                            {type}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <button className="GetRecords">Get Records</button>
+        </div>
+    );
 }
 
 export default Messages;
